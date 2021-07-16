2ac9743d12d479f8cdae6ae1936696a0511af7e3
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2021-07-15 20:23:01 -0500

    Database mapping to python

---
+++ mapping.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+
+engine = create_engine('postgresql://animalesitm:animalesitm@172.19.0.3:5432/animalesitm')
+
+m = MetaData(schema = 'bioacustica')
+Base = automap_base(bind=engine, metadata=m)
+
+Base.prepare(engine = engine, reflect=True)
+
+Evidence = Base.classes.evidence
+
