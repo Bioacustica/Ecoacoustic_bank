a47a836a1dd529c77bc042177416037dde214b06
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2021-10-12 13:55:18 -0500

    changing ip

--- mapping.py
+++ mapping.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 
-engine = create_engine('postgresql://animalesitm:animalesitm@172.19.0.2:5432/animalesitm')
+engine = create_engine('postgresql://animalesitm:animalesitm@172.19.0.3:5432/animalesitm')
 
 m = MetaData(schema = 'bioacustica')
 Base = automap_base(bind=engine, metadata=m)
