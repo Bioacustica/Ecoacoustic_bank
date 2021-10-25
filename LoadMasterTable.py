90b2c66695c81034d80ca48717710254c6f6cd72
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2021-10-25 18:11:01 -0500

    Adding CatalogueAdd part1

--- LoadMasterTable.py
+++ LoadMasterTable.py
@@ -91,9 +91,75 @@ SamplingAdd(file = "../UDAS_20210406.xls", session = session)
 
 #----------------------------------
 
+import datetime
+import pandas as pd
+from mapping import Base
+from mapping import engine
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+
+def CatalogueAdd(file, session, id):
+    
+    udas = pd.read_excel(file, sheet_name = "UDAS", header = 0)
+    
+    id_sampling = session.query(Base.classes["sampling"]).filter(Base.classes["sampling"].description == udas.iloc[id]["id_DM"]).first().id_sampling
+    id_country =  session.query(Base.classes["country"]).filter(Base.classes["country"].description == udas.iloc[id]["country"]).first().id_country
+    id_department = session.query(Base.classes["department"]).filter(Base.classes["department"].description == udas.iloc[id]["department"]).first().id_department
+    id_municipality = session.query(Base.classes["municipality"]).filter(Base.classes["municipality"].description == udas.iloc[id]["municipality"]).first().id_municipality
+    id_vereda = 
+    id_gain = session.query(Base.classes["gain"]).filter(Base.classes["gain"].description == udas.iloc[id]["gain"]).first().id_gain
+    id_collector = session.query(Base.classes["user"]).filter(Base.classes["user"].description == udas.iloc[id]["collector"]).first().id_user
+    id_h_serial = 
+    id_supply = session.query(Base.classes["user"]).filter(Base.classes["user"].description == udas.iloc[id]["collector"]).first().id_user
+    id_case = 
+    id_memory = 
+    id_habitad = 
+    id_precision = 
+    id_datum =
+    id_microphone = 
+    
+    id_project = session.query(Base.classes["project"]).filter(Base.classes["project"].description == udas.iloc[0]["project_name_PR"]).first().id_project
+    id_cataloger = session.query(Base.classes["user"]).filter(Base.classes["user"].email == udas.iloc[0]["collector_email_PR"]).first().id_user
+    
+    session.add(Base.classes["catalogue"](id_sampling = id_sampling,
+                                          id_country = id_country,
+                                          id_department = id_department,
+                                          id_municipality = id_municipality, 
+                                          id_vereda = id_vereda, 
+                                          id_gain = id_gain, 
+                                          id_collector = id_collector,
+                                          id_h_serial = id_h_serial,
+                                          id_supply = id_supply,
+                                          id_case = id_case,
+                                          id_memory = id_memory,
+                                          id_habitad = id_habitad,
+                                          id_precision = id_precision,
+                                          id_datum = id_datum,
+                                          id_microphone = id_microphone,
+                                          elevation = elevation, 
+                                          height = height,
+                                          
+                                          
+        id_project = id_project,
+                                          id_cataloger = id_cataloger,
+                                          id_season = id_season,
+                                          date = datetime.datetime.now(),
+                                          description = udas.iloc[0]["id_DM"]))
+    session.commit()
+
+
+
+SamplingAdd(file = "../UDAS_20210406.xls", session = session)
+
+
+
+#----------------------------------
+
 # Temas a discutir
 # 1) Dependencia entre project y funding
 # 2) Coherencia entre la información de las tablas maestras y la plantilla (tocaria agregar mas información a las tablas maestras o editarlas?)
 # 3) Como referenciar al usuario (por email, nombre o username)
 # 4) Hablar de la plantilla (separar por hojas (sampling, catalogo y registro)) es un UDAS por sampling
-# 5) nombre del sampling (nombre del estudio) iria en description
+# 5) Nombre del sampling (nombre del estudio) iria en description
