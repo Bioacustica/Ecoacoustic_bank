5d0c18f55cda3a72ac8819cedee841140352ef25
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2021-11-01 00:25:48 -0500

    Debug adding catalogue

--- LoadMasterTable.py
+++ LoadMasterTable.py
@@ -90,7 +90,7 @@ def SamplingAdd(file, session):
 SamplingAdd(file = "../UDAS_20210406.xls", session = session)
 
 #----------------------------------
-
+import os
 import datetime
 import pandas as pd
 from mapping import Base
@@ -102,56 +102,106 @@ session = Session(engine)
 
 def CatalogueAdd(file, session, id):
     
-    udas = pd.read_excel(file, sheet_name = "UDAS", header = 0)
+    udas = pd.read_excel(file, sheet_name = "Template", header = 0)
+    record_dir = udas.iloc[id]["record"]
+    infoDir = os.listdir(record_dir)
     
-    id_sampling = session.query(Base.classes["sampling"]).filter(Base.classes["sampling"].description == udas.iloc[id]["id_DM"]).first().id_sampling
-    id_country =  session.query(Base.classes["country"]).filter(Base.classes["country"].description == udas.iloc[id]["country"]).first().id_country
-    id_department = session.query(Base.classes["department"]).filter(Base.classes["department"].description == udas.iloc[id]["department"]).first().id_department
-    id_municipality = session.query(Base.classes["municipality"]).filter(Base.classes["municipality"].description == udas.iloc[id]["municipality"]).first().id_municipality
-    id_vereda = 
-    id_gain = session.query(Base.classes["gain"]).filter(Base.classes["gain"].description == udas.iloc[id]["gain"]).first().id_gain
-    id_collector = session.query(Base.classes["user"]).filter(Base.classes["user"].description == udas.iloc[id]["collector"]).first().id_user
-    id_h_serial = 
-    id_supply = session.query(Base.classes["user"]).filter(Base.classes["user"].description == udas.iloc[id]["collector"]).first().id_user
-    id_case = 
-    id_memory = 
-    id_habitad = 
-    id_precision = 
-    id_datum =
-    id_microphone = 
+    id_sampling = session.query(Base.classes["sampling"]). \
+                  filter(Base.classes["sampling"].description == udas.iloc[id]["sampling"]). \
+                  first().id_sampling
     
-    id_project = session.query(Base.classes["project"]).filter(Base.classes["project"].description == udas.iloc[0]["project_name_PR"]).first().id_project
-    id_cataloger = session.query(Base.classes["user"]).filter(Base.classes["user"].email == udas.iloc[0]["collector_email_PR"]).first().id_user
+    id_country =  session.query(Base.classes["country"]). \
+                  filter(Base.classes["country"].description == udas.iloc[id]["country"].replace('"','').upper()). \
+                  first().id_country
+    
+    id_department = session.query(Base.classes["department"]). \
+                    filter(Base.classes["department"].description == udas.iloc[id]["department"].replace('"','').upper()). \
+                    first().id_department
+    
+    id_m = session.query(Base.classes["municipality"]). \
+           filter(Base.classes["municipality"].description == udas.iloc[id]["municipality"].replace('"','').upper()). \
+           first().id_municipality
+    
+    id_vereda = 1
+    id_locality = 1
+    
+    id_gain = session.query(Base.classes["gain"]). \
+              filter(Base.classes["gain"].description == udas.iloc[id]["gain"].replace('"','').upper()). \
+              first().id_gain
+    
+    #id_filter = session.query(Base.classes["filter"]). \
+    #          filter(Base.classes["filter"].description == udas.iloc[id]["filter"].replace('"','').upper()). \
+    #          first().id_filter
+    id_filter = 1
+    
+    id_collector = session.query(Base.classes["user"]).filter(Base.classes["user"].email == udas.iloc[id]["collector"]).first().id_user
+    id_h_serial = 1
+    
+    id_supply = session.query(Base.classes["supply"]). \
+                filter(Base.classes["supply"].description == udas.iloc[id]["supply"].replace('"','').upper()). \
+                first().id_supply
+    
+    id_case = session.query(Base.classes["case"]). \
+              filter(Base.classes["case"].description == udas.iloc[id]["case"].replace('"','').upper()). \
+              first().id_case
+    
+    id_memory = session.query(Base.classes["memory"]). \
+                filter(Base.classes["memory"].description == udas.iloc[id]["memory"].replace('"','').upper()). \
+                first().id_memory
+    
+    id_habitat = session.query(Base.classes["habitat"]). \
+                 filter(Base.classes["habitat"].description == udas.iloc[id]["habitat"].replace('"','').upper()). \
+                 first().id_habitat
+    
+    id_precision = session.query(Base.classes["precision"]). \
+                   filter(Base.classes["precision"].description == udas.iloc[id]["precision"].replace('"','').upper()). \
+                   first().id_precision
+    
+    id_datum = session.query(Base.classes["datum"]). \
+               filter(Base.classes["datum"].description == udas.iloc[id]["datum"].replace('"','').upper()).  \
+               first().id_datum
+    
+    id_microphone = session.query(Base.classes["microphone"]). \
+                    filter(Base.classes["microphone"].description == udas.iloc[id]["microphone"].replace('"','').upper()). \
+                    first().id_microphone
+    
+    elevation = udas.iloc[id]["elevation"]
+    height = udas.iloc[id]["height"]
+    chunks = len(infoDir)
+    size = 1
+    latitude = udas.iloc[id]["latitude"]
+    longitude = udas.iloc[id]["longitude"]
+    description = udas.iloc[id]["catalogue"]
     
     session.add(Base.classes["catalogue"](id_sampling = id_sampling,
                                           id_country = id_country,
                                           id_department = id_department,
-                                          id_municipality = id_municipality, 
+                                          id_municipality = id_m, 
                                           id_vereda = id_vereda, 
+                                          id_locality = id_locality,
                                           id_gain = id_gain, 
+                                          id_filter = id_filter,
                                           id_collector = id_collector,
                                           id_h_serial = id_h_serial,
                                           id_supply = id_supply,
                                           id_case = id_case,
                                           id_memory = id_memory,
-                                          id_habitad = id_habitad,
+                                          id_habitat = 1,
                                           id_precision = id_precision,
                                           id_datum = id_datum,
                                           id_microphone = id_microphone,
-                                          elevation = elevation, 
-                                          height = height,
-                                          
-                                          
-        id_project = id_project,
-                                          id_cataloger = id_cataloger,
-                                          id_season = id_season,
-                                          date = datetime.datetime.now(),
-                                          description = udas.iloc[0]["id_DM"]))
+                                          #elevation = pd.to_numeric(elevation), 
+                                          #height = pd.to_numeric(height),
+                                          #chunks = pd.to_numeric(chunks),
+                                          #size = pd.to_numeric(size, downcast='float'),
+                                          #latitude = pd.to_numeric(latitude, downcast='float'),
+                                          #longitude = pd.to_numeric(longitude, downcast='float'),
+                                          description = description))
     session.commit()
 
 
 
-SamplingAdd(file = "../UDAS_20210406.xls", session = session)
+CatalogueAdd(file = "../UDAS_20210406.xls", session = session, id = 1)
 
 
 
