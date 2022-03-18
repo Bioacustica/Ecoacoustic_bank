f17844bb7e79d365c0f022b62ff4c885f4421316
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-03-17 23:18:48 -0500

    Add Sampling is create in an independent file

---
+++ AddSampling.py
@@ -0,0 +1,42 @@
+import datetime
+import pandas as pd
+from mapping import Base
+from mapping import engine
+from sqlalchemy.orm import Session
+
+session = Session(engine)
+
+
+def SamplingAdd(udas, session, id):
+    
+    try:
+        id_season = session.query(Base.classes["season"]).filter(Base.classes["season"].description == udas.iloc[id]["season_HA"]).first().id_season
+        id_project = session.query(Base.classes["project"]).filter(Base.classes["project"].description == udas.iloc[id]["project_name_PR"]).first().id_project
+        id_cataloger = session.query(Base.classes["user"]).filter(Base.classes["user"].email == udas.iloc[id]["collector_email_PR"]).first().id_user
+        
+        session.add(Base.classes["sampling"](id_project = id_project,
+                                            id_cataloger = id_cataloger,
+                                            id_season = id_season,
+                                            date = datetime.datetime.now(),
+                                            description = udas.iloc[id]["id_DM"]))
+        session.commit()
+    except:
+        if not 'id_season' in locals():
+            print("ERROR: season_HA - " + str(id+1) + " ->  " + str(udas.iloc[id]["season_HA"]) )
+        elif not 'id_project' in locals():
+            print("ERROR: project_name_PR - " + str(id+1) + " ->  " + str(udas.iloc[id]["project_name_PR"]) )
+        elif not 'id_cataloger' in locals():
+            print("ERROR: collector_email_PR - " + str(id+1) + " ->  " + str(udas.iloc[id]["collector_email_PR"]) )    
+
+
+
+def SamplingsAdd(file, session):
+    
+    udas = pd.read_excel(file, sheet_name = "UDAS", header = 0)
+    
+    for id in range(udas.shape[0]):
+        
+        SamplingAdd(udas, session, id)
+
+
+SamplingsAdd(file = '/home/andres/Proyectos/Software/Bioacustico/bioacustica/UDAS_20210406.xls', session = session)
