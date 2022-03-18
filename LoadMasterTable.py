92833b285086c321739e2ea6322007c3c032b3ce
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-03-17 23:20:17 -0500

    Add Sampling is removed of LoadMasterTable

--- LoadMasterTable.py
+++ LoadMasterTable.py
@@ -65,41 +65,6 @@ session.commit()
 
 #---------------------------------
 
-import datetime
-import pandas as pd
-from mapping import Base
-from mapping import engine
-from sqlalchemy.orm import Session
-
-session = Session(engine)
-
-
-def SamplingAdd(udas, session, id):
-    
-    id_season = session.query(Base.classes["season"]).filter(Base.classes["season"].description == udas.iloc[id]["season_HA"]).first().id_season
-    id_project = session.query(Base.classes["project"]).filter(Base.classes["project"].description == udas.iloc[id]["project_name_PR"]).first().id_project
-    id_cataloger = session.query(Base.classes["user"]).filter(Base.classes["user"].email == udas.iloc[id]["collector_email_PR"]).first().id_user
-    
-    session.add(Base.classes["sampling"](id_project = id_project,
-                                         id_cataloger = id_cataloger,
-                                         id_season = id_season,
-                                         date = datetime.datetime.now(),
-                                         description = udas.iloc[id]["id_DM"]))
-    session.commit()
-
-
-def SamplingsAdd(file, session):
-    
-    udas = pd.read_excel(file, sheet_name = "UDAS", header = 0)
-    
-    for id in range(udas.shape[0]):
-        
-        SamplingAdd(udas, session, id)
-
-
-SamplingsAdd(file = "../UDAS_20210406.xls", session = session, id = 0)
-
-#----------------------------------
 import os
 import datetime
 import pandas as pd
