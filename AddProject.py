f171a8efd0908edaedf2984a4fe30c691e7ec45b
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-03-21 23:30:16 -0500

    Avoid error with nan when uppercase is done

--- AddProject.py
+++ AddProject.py
@@ -8,7 +8,7 @@ session = Session(engine)
 def AddProject(udas, session, id):
     
     try:
-        funding = udas.iloc[id]["funding_PR"].upper()
+        funding = str(udas.iloc[id]["funding_PR"]).upper()
         id_funding = session.query(Base.classes["funding"]).filter(Base.classes["funding"].description == funding).first().id_funding
         session.add(Base.classes["project"](id_funding = id_funding,
                                             description = udas.iloc[id]["project_name_PR"]))
