de9ae67be22c25afbd1619b6d60cb89cf7fff925
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-03-21 23:03:45 -0500

    A main fuction was created in order to load UDAS

---
+++ LoadData.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from mapping import Base
+from mapping import engine
+from sqlalchemy.orm import Session
+
+from AddProject import AddProjects_
+from AddSampling import AddSamplings_
+from AddCatalogue import AddCatalogues_
+from AddRecord import AddRecords_
+
+
+session = Session(engine)
+
+
+def LoadData(file, session):
+    
+    print("Stage 1: Projects")
+    AddProjects_(file, session)
+    
+    print("Stage 2: Samplings")
+    AddSamplings_(file, session)
+    
+    print("Stage 3: Catalogues")
+    AddCatalogues_(file, session)
+    
+    print("Stage 4: Records")
+    AddRecords_(file, session)
+
+
+
+LoadData(file = '/home/andres/Proyectos/Software/Bioacustico/bioacustica/UDAS_20210406.xls',
+         session = session) 
+
