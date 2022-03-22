d78d882a012206476e66860f87303b983a2e3d4f
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-03-21 23:24:35 -0500

    the query in Id_funding is done with uppercase

--- AddProject.py
+++ AddProject.py
@@ -8,11 +8,12 @@ session = Session(engine)
 def AddProject(udas, session, id):
     
     try:
-        id_funding = session.query(Base.classes["funding"]).filter(Base.classes["funding"].description == udas.iloc[id]["funding_PR"]).first().id_funding
+        funding = udas.iloc[id]["funding_PR"].upper()
+        id_funding = session.query(Base.classes["funding"]).filter(Base.classes["funding"].description == funding).first().id_funding
         session.add(Base.classes["project"](id_funding = id_funding,
                                             description = udas.iloc[id]["project_name_PR"]))
     except:
-        print("ERROR: funding_PR - " + str(id+1) + " ->  " + str(udas.iloc[id]["funding_PR"]) )
+        print("ERROR: funding_PR - " + str(id+1) + " ->  " + str(funding) )
 
 
 
