777d687307a1228e58516ef0242083dfadc3215f
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-06-09 16:44:32 -0500

    Creating dummy user

--- AddUser.py
+++ AddUser.py
@@ -5,9 +5,15 @@ from sqlalchemy.orm import Session
 
 session = Session(engine)
 
-session.add(Base.classes["user"](name = "JUAN MANUEL",
-                                 email = "juanm.daza@udea.edu.co",
-                                 username = "juanm.daza",
+# session.add(Base.classes["user"](name = "JUAN MANUEL",
+#                                  email = "juanm.daza@udea.edu.co",
+#                                  username = "juanm.daza",
+#                                  roles = "ADMINISTRADOR"))
+
+session.add(Base.classes["user"](name = "ESTEFANY CANO",
+                                 email = "canoestefany@gmail.com",
+                                 username = "estefany.cano",
                                  roles = "ADMINISTRADOR"))
+
 session.commit()
 
