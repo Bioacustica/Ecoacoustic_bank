aea2131a9a33df95b095f3515f7340e6211263d0
beowoulfk <victor.vt649@gmail.com>
2022-08-09 18:40:05 -0500

    removed __pycache__ and .ipynb_checkpoints

--- LoadData.py
+++ LoadData.py
@@ -21,15 +21,6 @@ def LoadData(file, session):
 
     print("Stage 1: Projects")
     AddProjects_(file, session)
-<<<<<<< HEAD
-
-    print("Stage 2: Samplings")
-    AddSamplings_(file, session)
-
-    print("Stage 3: Catalogues")
-    AddCatalogues_(file, session)
-
-=======
     VerifyStage(session)
     
     print("Stage 2: Samplings")
@@ -40,31 +31,12 @@ def LoadData(file, session):
     AddCatalogues_(file, session)
     VerifyStage(session)
     
->>>>>>> cda1a6a1114c67153389ea076204e801f7f5161c
     print("Stage 4: Records")
     AddRecords_(file, session)
     VerifyStage(session)
 
     print(" ")
 
-<<<<<<< HEAD
-    if Globals.Bug:
-        session.rollback()
-        print("Reversing transaction")
-        print("Generating: Master Table file")
-        GenerateMasterTable(file="MasterTablesGenerada_.xlsx", session=session)
-    else:
-        session.commit()
-        print("Successful transaction")
-
-    Globals.Bug = False
-    session.close()
-
-
-LoadData(file="./Tesis_Cano_20211026.xls", session=session)
-# LoadData(file = '/home/andres/Proyectos/Software/Bioacustico/UDAS_CSG_2018.xls',
-#         session = session)
-=======
     session.commit()
     print("Successful transaction")
     session.close()
@@ -77,7 +49,6 @@ LoadData(file = '/home/andres/Proyectos/Software/Bioacustico/nuevaPrueba/Ultraso
 #        session = session) 
 #LoadData(file = '/home/andres/Proyectos/Software/Bioacustico/UDAS_CSG_2018.xls',
 #         session = session) 
->>>>>>> cda1a6a1114c67153389ea076204e801f7f5161c
 
 
 # incrementar el ID que se imprime (ok)
@@ -92,11 +63,8 @@ LoadData(file = '/home/andres/Proyectos/Software/Bioacustico/nuevaPrueba/Ultraso
 # TO-DO
 # Se debe generar id_h_serial (en esta oportunidad se genero manual desde el pgadmin)
 # como hacer la consulta para obtener el id_record tiene que crearse primero en la base de datos
-<<<<<<< HEAD
-=======
 # unificar los nombres de las tablas y del udas
 # tener cuidado con llenar los campos numericos, no se puede colocar no se conoce (definir por defecto un numero)
 
 # el project no se crea como tabla maestra, se crea con loadData ya que este depende  de id_funding
 # verificar si el record fue creado ahora solo se hace a nivel de record_path
->>>>>>> cda1a6a1114c67153389ea076204e801f7f5161c
