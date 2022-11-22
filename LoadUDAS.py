1b054ba90403c96780f9e4fe3d8fec7930cb11ca
server mirp <andresgiraldo3312@correo.itm.edu.com>
2022-11-22 15:37:44 -0500

    Expose in port 7001 loadMasterTable. It is necessary run LoadUDAS.py

--- LoadUDAS.py
+++ LoadUDAS.py
@@ -1,18 +1,23 @@
-#!/usr/local/bin/python3
-#TODO FAlta terminarlo
-
-import argparse
-
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-
-parser.add_argument('--UDAS', type = str, 
-                    help = 'Path UDAS file')
-
-
-args = parser.parse_args()
-
-print(args.UDAS)
+import pandas as pd
+from mapping import Base
+from mapping import engine
+from sqlalchemy.orm import Session
+import LoadMasterTable as LMT
+from flask import Flask, request, jsonify
 
 session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route('/LoadMasterTables', methods = ['POST'])
+def upload_file():
+    file = request.files['file']
+    file.save("MasterTables_v1.xlsx")
+    LMT.LoadMasterTables(info_path = 'MasterTables_v1.xlsx',
+                         mapping = Base.classes,
+                         engine = engine)
+    return "done"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7001)
 
