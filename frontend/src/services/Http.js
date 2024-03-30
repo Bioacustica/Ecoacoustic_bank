ce20dfbdd4e41953538bbf2c89a004c9f942dbe8
andres <andres_PCpersonal>
2024-03-30 16:17:16 -0500

    [200~change for running in localhost~

--- frontend/src/services/Http.js
+++ frontend/src/services/Http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Http = axios.create({
-  baseURL: "http://192.168.0.2:8000/",
+  baseURL: "http://localhost:8000/",
   timeout: 10000,
   headers: {},
 });
